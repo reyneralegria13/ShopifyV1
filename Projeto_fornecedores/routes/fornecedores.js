3d2503e664422502eda70a8cc23319453a1b0bb1
Reyner Alegria <reyneralegria18@gmail.com>
2024-11-12 20:44:53 -0400

    toma mais

--- Projeto_fornecedores/routes/fornecedores.js
+++ Projeto_fornecedores/routes/fornecedores.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Fornecedor = require('../Models/fornecedor');
+const Fornecedor = require('../models/fornecedor');
 
 // Listar fornecedores
 router.get('/', async (req, res) => {
